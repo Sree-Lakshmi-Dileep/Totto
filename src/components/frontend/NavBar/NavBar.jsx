21c7bc838a356405d4c48a2e7288289e7d48ad91
Sree-Lakshmi-Dileep <sreelakshmidileep2004@gmail.com>
2025-05-14 09:07:28 +0530

    nav-css2

--- src/components/frontend/NavBar/NavBar.jsx
+++ src/components/frontend/NavBar/NavBar.jsx
@@ -8,38 +8,17 @@ function NavBar() {
         <div className="logo">
           <img src="/logo.png" className='logos' alt="logo" />
         </div>
-        <div className="leftb">
+        <div className="lefta">
           <div className="search">
-            <input type="text" placeholder='what are rou looking for.....' />
+            <input className='input' type="text" placeholder='what are rou looking for.....' />
          </div>
-      <div className="nav-links">
-         <div className='item'>
-           <span className='icon'> <img className='imgicon' src="https://cdn-icons-png.flaticon.com/512/4383/4383084.png" alt="Skincare Icon" />   
-          </span>
-          <span>skincare</span>
-         </div>
+         <div className="nav-links">
+              <button className='button'>HOME</button>
+             <button className='button'>ABOUT</button>
+              <button className='button'>TOYS</button>
 
-         <div className='item'>
-          <span className='icon'>
-            <img className='imgicon' src="https://cdn-icons-png.flaticon.com/512/10084/10084943.png" alt="" /> 
-         </span>
-         <span>makeup</span>
-         </div>
 
-         <div className="item">
-          <span className='icon'>
-            <img className='imgicon' src="https://cdn-icons-png.flaticon.com/512/3299/3299031.png" alt="" />
-          </span>
-          <span>haircare</span>
-         </div>
-
-         <div className="item">
-          <span className='icon'>
-            <img className='imgicon' src="https://cdn-icons-png.flaticon.com/512/2047/2047460.png" alt="" />
-          </span>
-          <span>perfume</span>
-         </div>
-       </div>
+          </div>
         </div>
         
       </div>
