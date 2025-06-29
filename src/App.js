bf45027c2ba9aa1e09e06f725672cbc0d0609a43
Sree-Lakshmi-Dileep <sreelakshmidileep2004@gmail.com>
2025-06-29 21:57:28 +0530

    login page design

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, useLocation } from 'react-router-dom';
+
 import NavBar from './components/frontend/NavBar/NavBar';
 import Home from './components/frontend/Home/Home';
 import About from './components/frontend/About/About';
@@ -11,30 +12,42 @@ import Forgotpass from './components/frontend/forgotpass/forgotpass';
 import FavouritesPage from './components/frontend/NavBar/favourites';
 import Cart from './components/frontend/NavBar/cart';
 
+function AppContent() {
+  const location = useLocation();
 
+  // Paths where NavBar should be hidden
+  const hideNavRoutes = ['/', '/login', '/signup', '/forgot-password'];
+  const showNavBar = !hideNavRoutes.includes(location.pathname);
 
-
-
-function App() {
   return (
-    <Router>
-      <NavBar />
+    <>
+      {showNavBar && <NavBar />}
+
       <Routes>
+        {/* Auth Routes - No NavBar */}
+        <Route path="/" element={<Login />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/forgot-password" element={<Forgotpass />} />
+
+        {/* Main Routes - With NavBar */}
         <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/toys" element={<Toys />} />
         <Route path="/toys/:toyId" element={<ToyDetails />} />
         <Route path="/category" element={<Category />} />
         <Route path="/category/:categoryName" element={<Category />} />
-        <Route path="/" element={<Login/>}/>
-        <Route path="/login" element={<Login/>}/>
-        <Route path="/signup" element={<Signup />} />
-        <Route path="/forgot-password" element={<Forgotpass />} />
-<Route path="/favourites" element={<FavouritesPage />} />
-        <Route path="/cart" element={<Cart/>}/>
-
-
+        <Route path="/favourites" element={<FavouritesPage />} />
+        <Route path="/cart" element={<Cart />} />
       </Routes>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <AppContent />
     </Router>
   );
 }
--- src/components/frontend/Login/Login.jsx
+++ src/components/frontend/Login/Login.jsx
@@ -6,30 +6,22 @@ function Login() {
   return (
     <div className="div1">
       <div className="div2">
-        
-        
-
         <h2 className="l_head">Login</h2>
         <form>
-          <div className="d_email">
-            <label className="l_email">Email </label>
-            <input type="email" className="i_email" />
-          </div>
-          <div className="d_pass">
-            <label className="l_pass">Password  </label>
-            <input type="password" className="i_pass" />
+          <div className="d_form">
+            <input type="email" className="i_email" placeholder='Email Address'/>
+            <input type="password" className="i_pass"  placeholder='password'/>
           </div>
           <button type="submit" className="submitbutton">
             Login
           </button>
           <div className="log_link">
             <Link to="/home" className="skip-button">⏮️ Skip</Link>
-           <Link to="/forgot-password">Forgot password?</Link>         </div>
-
+            <Link to="/forgot-password">Forgot password?</Link>        
+          </div>
         </form>
-
         <p className="sign">
-          Don't have an account? <Link to="/signup">Sign up for Totto</Link>
+          Don't have an account? <Link to="/signup" className='signlink'>Sign up for Totto</Link>
         </p>
       </div>
     </div>
